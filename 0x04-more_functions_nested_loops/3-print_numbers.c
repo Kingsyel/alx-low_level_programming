e6879aafe86e2db455d181a58c442ef6f07b61f0
Kingsyel <kingsleyotieno2013@gmail.com>
2022-03-18 22:48:06 +0300

    my commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,13 @@
 /**
  * print_numbers - prints the numbers,followed by new line
  *
- *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int a;
 
-	if (a = 0, a < 10, a++)
+	if (a = 0; a < 10; a++)
 	{ _puthcar (a);
 	}
 	_putchar ('\n');
