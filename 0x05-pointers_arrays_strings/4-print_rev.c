7efaff988de314fb54409686815f7471ff9e7448
Kingsyel <kingsleyotieno2013@gmail.com>
2022-03-22 23:19:51 +0300

    my commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: a pointer to an int that will be updated
+ *
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i -1 ; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+
+	_putchar ('\n')
+}
