d2e39dc5d60d5acbcfa0c2167384331309d3476b
Kingsyel <kingsleyotieno2013@gmail.com>
2022-03-22 22:20:42 +0300

    my commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: pointer to an integer
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98
+}
+
